82605a82783b98c107c8c56d2343dd2fe2a910ea
Roman <elgoog6651@gmail.com>
2020-09-29 01:10:26 +0300

    part 3 finished

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import *
 from django.http import JsonResponse
 import json
+import datetime
+from .models import *
 
 def store(request):
      if request.user.is_authenticated:
@@ -67,3 +68,30 @@ def updateItem(request):
 
      return JsonResponse('Item was added', safe=False)
 
+def processOrder(request):
+     transaction_id = datetime.datetime.now().timestamp()
+     data = json.loads(request.body)
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          total = float(data['form']['total'])
+          order.transaction_id = transaction_id
+
+          if total == order.get_cart_total:
+               order.complete = True
+          order.save()
+
+          if order.shipping == True:
+               ShippingAdress.objects.create(
+                    customer=customer,
+                    order=order,
+                    address=data['shipping']['address'],
+                    city=data['shipping']['city'],
+                    state=data['shipping']['state'],
+                    zipcode=data['shipping']['zipcode'],
+               )
+
+     else:
+          print('User is not logged in...')
+     return JsonResponse('Payment complete', safe=False)
