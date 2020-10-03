f282c8e0488c105b7712ca510dc17fe68cbc171c
Roman <elgoog6651@gmail.com>
2020-10-03 16:23:52 +0300

    dilate probils

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,29 +5,25 @@ import datetime
 from .models import *
 from . utils import *
 
+
 def store(request):
-    
      data = cartData(request)
      cartItems = data['cartItems'] 
-
      products = Product.objects.all()
      context = {'products':products, 'cartItems':cartItems}
      return render(request, 'store/store.html', context)
 
 
 def cart(request):
-   
      data = cartData(request)
      cartItems = data['cartItems']
      order = data['order']
      items = data['items']
-          
      context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-
      data = cartData(request)
      cartItems = data['cartItems']
      order = data['order']
@@ -45,10 +41,8 @@ def updateItem(request):
      data = json.loads(request.body)
      productId = data['productId']
      action = data['action']
-     
      print('Action:', action)
      print('productId:', productId)
-
      customer = request.user.customer
      product = Product.objects.get(id=productId)
      order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -60,33 +54,22 @@ def updateItem(request):
      orderItem.save()
      if orderItem.quantity <= 0:
           orderItem.delete()
-     
-
      return JsonResponse('Item was added', safe=False)
 
 
 def processOrder(request):
      transaction_id = datetime.datetime.now().timestamp()
      data = json.loads(request.body)
-
      if request.user.is_authenticated:
           customer = request.user.customer
           order, created = Order.objects.get_or_create(customer=customer, complete=False)
-         
-
-         
-
      else:
           customer, order = guestOrder(request, data)
-          
-
      total = float(data['form']['total'])
      order.transaction_id = transaction_id
-
      if total == order.get_cart_total:
           order.complete = True
      order.save()
-
      if order.shipping == True:
           ShippingAdress.objects.create(
                customer=customer,
@@ -94,9 +77,7 @@ def processOrder(request):
                address=data['shipping']['address'],
                city=data['shipping']['city'],
                state=data['shipping']['state'],
-               zipcode=data['shipping']['zipcode'],
-          )
-
+               zipcode=data['shipping']['zipcode'],)
      return JsonResponse('Payment complete', safe=False)
 
 
