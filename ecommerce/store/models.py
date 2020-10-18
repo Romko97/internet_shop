c542bf1317bac930361e1a7aa77a7c8b359b79ed
Roman <elgoog6651@gmail.com>
2020-10-18 21:17:19 +0300

    add size to table

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
+    size = models.CharField(max_length=200, null=True)
     image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
